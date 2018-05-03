3185d8cdb1371f7f561d133a98d82b22c7b392d1
Arnab Bir <arnabbir@gmail.com>
2018-05-03 14:30:58 +0530

    Update HISTOGRA.cpp

--- HISTOGRA.cpp
+++ HISTOGRA.cpp
@@ -4,23 +4,27 @@
 
 using namespace std;
 
-long long min_sgtree(long long * h, int l, int r, long long * sgtr, int index){
+int min_sgtree(long long * h, int l, int r, int * sgtr, int index){
     
     if(l == r){
         
-        sgtr[index] = h[l];
+        sgtr[index] = l;
         return sgtr[index];
     }
     
     int mid = l + (r-l)/2;
     
-    sgtr[index] = min(min_sgtree(h, l, mid, sgtr, 2 * index + 1), min_sgtree(h, mid+1, r, sgtr, 2 * index + 2));
+    int left = min_sgtree(h, l, mid, sgtr, 2 * index + 1);
+    int right = min_sgtree(h, mid+1, r, sgtr, 2 * index + 2);
+    
+    sgtr[index] = ( h[left] < h[right]) ? left : right;
+    //sgtr[index] = min(min_sgtree(h, l, mid, sgtr, 2 * index + 1), min_sgtree(h, mid+1, r, sgtr, 2 * index + 2));
     return sgtr[index];
 }
 
-int get_min_in_range(long long * h, int p, int q, int l, int r, long long * sgtr, int index){
+int get_min_in_range(long long * h, int p, int q, int l, int r, int * sgtr, int index){
     
-    if(p <= l && r <= q)    return index;
+    if(p <= l && r <= q)    return sgtr[index];
     if(p > r || q < l)  return -1;
     
     int mid = l + (r-l) / 2;
@@ -28,18 +32,18 @@ int get_min_in_range(long long * h, int p, int q, int l, int r, long long * sgtr
     int left = get_min_in_range(h, p, q, l, mid, sgtr, 2*index + 1);
     int right = get_min_in_range(h, p, q, mid+1, r, sgtr, 2*index + 2);
     
-    if(sgtr[left] < sgtr[right] || right == -1)  return left;
+    if(h[left] < h[right] || right == -1)  return left;
     else return right;
 }
 
-long long  get_max_area(long long  * h, int n, int l, int r, long long * sgtr){
+long long  get_max_area(long long  * h, int n, int l, int r, int * sgtr){
     
     if(l+1 == r)  return h[l];
     if(l+1 > r)   return 0;
     
     
     int index = get_min_in_range(h, l, r-1, 0, n-1, sgtr, 0);
-    long long  min_h = sgtr[index];    
+    //int  min_h = sgtr[index];    
     
      /*for(int i = l+1; i < r; ++i){
 	        
@@ -50,7 +54,7 @@ long long  get_max_area(long long  * h, int n, int l, int r, long long * sgtr){
 	        }
 	    }*/
 	 
-    return max(min_h * (r-l), max(get_max_area(h, n, l, index, sgtr), get_max_area(h, n, index+1, r, sgtr)));
+    return max(h[index] * (r-l), max(get_max_area(h, n, l, index, sgtr), get_max_area(h, n, index+1, r, sgtr)));
 }
 
 int main() {
@@ -70,7 +74,7 @@ int main() {
 	        
 	        cin>>h[i];
 	    }
-	    long long * sgtr = new long long[2*(int)pow(2, ceil(n))];
+	    int * sgtr = new int[2*(int)pow(2, ceil(n))];
 	    min_sgtree(h, 0, n-1, sgtr, 0);
 	    cout<<get_max_area(h, n, 0, n, sgtr)<<endl;
 	}
