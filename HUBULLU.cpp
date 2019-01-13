7f1339d81b6828c9a9e2a13f77ab9901a8be8a18
Arnab Bir <arnabbir@gmail.com>
2019-01-13 22:10:41 +0530

    Create HUBULLU.cpp

---
+++ HUBULLU.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int t, n, p, count;
+	cin>>t;
+	while(t--) {
+	    cin>>n>>p;
+	    cout<<((!p)  ? "Airborne wins." : "Pagfloyd wins.")<<endl;
+	}
+	return 0;
+}
