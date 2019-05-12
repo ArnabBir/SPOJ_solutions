fec7f39f58c142b90c3751030e484d5e706f7cf6
Arnab Bir <arnabbir@gmail.com>
2019-05-12 12:40:19 +0530

    Create TRICOUNT.cpp

---
+++ TRICOUNT.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        unsigned long long n;
+        scanf("%llu",&n);
+        unsigned long long ans;
+        ans=(n*(n+2)*(2*n+1)/8);
+        printf("%llu\n",ans);
+    }
+    return 0;
+}
